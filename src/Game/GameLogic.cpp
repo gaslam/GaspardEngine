cb9bc27d5d9099bc8a98ef24afb4b0d4f1f4d2ad
gaslam <44768183+gaslam@users.noreply.github.com>
2025-09-25 16:26:41 +0200

    Init GLFW window

--- src/Game/GameLogic.cpp
+++ src/Game/GameLogic.cpp
@@ -1,4 +1,5 @@
 #include "Game/GameLogic.h"
+#include <format>
 
 namespace GaspardEngine {
 
@@ -6,8 +7,9 @@ namespace GaspardEngine {
 		m_LogManager = std::make_unique<LogManager>();
 		m_LogManager->Init(L"GameLogger");
 	};
-	void Game::GameLogic(float) {
-		m_LogManager->LogInfo(L"test");
+	void Game::GameLogic(float deltaTime) {
+		auto test{ std::format(L"deltaTime {}", deltaTime) };
+		m_LogManager->LogInfo(test.c_str());
 	};
 	void Game::Close() noexcept
 	{
--- src/platforms/glfwMain.cpp
+++ src/platforms/glfwMain.cpp
@@ -1,19 +1,46 @@
 #include <Game/GameLogic.h>
+#include <chrono>
 int main()
 {
 	//this is temporary
 	//TODO: replace this with window while loop
-	bool testWhileLogic{ true };
+	glfwInit();
+	glfwWindowHint(GLFW_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_VERSION_MINOR, 3);
+
+	GaspardEngine::LogManager Manager{};
+	Manager.Init(L"Program manager");
+
+	GLFWwindow* pWindow{ glfwCreateWindow(800,800,"GaspardEngine",NULL,NULL) };
+
+	if (pWindow == NULL)
+	{
+		Manager.LogError(L"Cannot create GLFW window.");
+		glfwTerminate();
+		return -1;
+	}
 
 	GaspardEngine::Game Game{};
 	Game.InitializeGame();
 
-	while (testWhileLogic)
+	glfwMakeContextCurrent(pWindow);
+
+	gladLoadGL();
+
+	glViewport(0, 0, 800, 800);
+
+	std::chrono::high_resolution_clock::time_point start{ std::chrono::high_resolution_clock::now() };
+	std::chrono::high_resolution_clock::time_point end{ };
+	while (!glfwWindowShouldClose(pWindow))
 	{
-		Game.GameLogic(0.0f);
-		testWhileLogic = false;
+		end = std::chrono::high_resolution_clock::now();
+		float deltaTime{ std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() / 1000000000.f };
+		start = std::chrono::high_resolution_clock::now();
+		Game.GameLogic(deltaTime);
 	}
 	std::cin.ignore();
 	Game.Close();
+	glfwDestroyWindow(pWindow);
+	glfwTerminate();
 	return 0;
 }
